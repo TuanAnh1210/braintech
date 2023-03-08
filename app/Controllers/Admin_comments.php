766f798602da47fc5e0c7b2d3e3113b085836fd6
TuanAnhNguyen <tuananh1210085213@gmail.com>
2023-03-08 20:49:07 +0700

    feat: handle active sidebar

--- app/Controllers/Admin_comments.php
+++ app/Controllers/Admin_comments.php
@@ -1,7 +1,14 @@
-<?php 
-    class Admin_comments extends BaseController {
+<?php
+class Admin_comments extends BaseController
+{
 
-        public function index() {
-            return $this -> view("admin.pages.comments.index");
-        }
+    public function index()
+    {
+        return $this->view("admin.pages.comments.index");
     }
+
+    public function detailCmt()
+    {
+        return $this->view("admin.pages.comments.detailCmt");
+    }
+}
