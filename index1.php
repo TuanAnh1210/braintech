2085324569a7ec3912ad52da81e3f50d71eedf12
dungtqph29145 <dungtqph29145@fpt.edu.vn>
2023-03-21 10:27:07 +0700

    update

---
+++ index1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="wrapper w-[full] max-w-[1440px] mx-auto my-auto">
+        <div class="header h-[90px] mt-3">
+            <div class="flex items-center gap-4">
+                <img class="w-[50px] h-[50px] rounded-md" src="./public/imgs/logo.png" alt="">
+                <p>Brain Tech Edu</p>
+            </div>
+            <div class="menu">
+                <ul>
+                    <li><a href="">Trang chu</a></li>
+                    <li><a href="">Khoa hoc</a></li>
+                    <li><a href="">Gioi thieu</a></li>
+                    <li><a href="">Lien he</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
