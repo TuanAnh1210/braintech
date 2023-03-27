8e6539174b873eb713af66b686e1cbb4f67cc734
TuanAnhNguyen <tuananh1210085213@gmail.com>
2023-03-27 20:44:49 +0700

    feat: full statistical

--- app/Views/admin/pages/statistical/index.php
+++ app/Views/admin/pages/statistical/index.php
@@ -4,26 +4,24 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="card">
-                    <div style="display: flex; justify-content: space-between; align-items: center;"
-                        class="card-header card-header-primary">
-                        <button class="courses-add">
-                            <a style="height: 100%; display: flex; align-items: center; width: 100%;"
-                                href="<?= $GLOBALS['domainPage'] ?>/admin_courses/addCourse?cateId=1">Biểu đồ</a>
-                        </button>
+                    <div style="display: flex; justify-content: space-between;" class="card-header card-header-primary">
+
                         <div class="courses-heading">
                             <h4 class="card-title ">Khóa học Front-End</h4>
-                            <!-- <p class="card-category"> Thống kê</p> -->
+                            <p class="card-category"> Thống kê</p>
                         </div>
 
-
-                        <div class="filter-wrapper">
+                        <button class="courses-add">
+                            <a style="height: 100%; display: flex; align-items: center; width: 100%;" href="<?= $GLOBALS['domainPage'] ?>/admin_courses/addCourse?cateId=1">Biểu đồ</a>
+                        </button>
+                        <!-- <div class="filter-wrapper">
                             <p class="filter-icon"><i class="material-icons">filter_alt</i>Sắp xếp</p>
                             <ul class="filter-sub">
-                                <li class="filter-item">Nhiều học viên nhất</li>
-                                <li class="filter-item">Nhiều chương nhất</li>
-                                <li class="filter-item">Nhiều bài học nhất</li>
+                                <li data-filter="learner" class="filter-item fe-filter">Nhiều học viên nhất</li>
+                                <li data-filter="chapter" class="filter-item fe-filter">Nhiều chương nhất</li>
+                                <li data-filter="lesson" class="filter-item fe-filter">Nhiều bài học nhất</li>
                             </ul>
-                        </div>
+                        </div> -->
 
                     </div>
                     <div class="card-body">
@@ -71,8 +69,7 @@
                             <p class="card-category"> Thống kê</p>
                         </div>
                         <button class="courses-add">
-                            <a style="height: 100%; display: flex; align-items: center; width: 100%;"
-                                href="<?= $GLOBALS['domainPage'] ?>/admin_courses/addCourse?cateId=2">Biểu đồ</a>
+                            <a style="height: 100%; display: flex; align-items: center; width: 100%;" href="<?= $GLOBALS['domainPage'] ?>/admin_courses/addCourse?cateId=2">Biểu đồ</a>
                         </button>
                     </div>
                     <div class="card-body">
@@ -118,79 +115,78 @@
 
 
 <script>
-// handle get data from db and convert arr php to arr js
-const courseFe = <?= json_encode($courseFe) ?>;
-const courseBe = <?= json_encode($courseBe) ?>;
-const chapters = <?= json_encode($chapters) ?>;
-const detail_courses = <?= json_encode($detail_courses) ?>;
-const domainPage = <?= json_encode($GLOBALS['domainPage']) ?>;
+    // handle get data from db and convert arr php to arr js
+    const courseFe = <?= json_encode($courseFe) ?>;
+    const courseBe = <?= json_encode($courseBe) ?>;
+    const chapters = <?= json_encode($chapters) ?>;
+    const detail_courses = <?= json_encode($detail_courses) ?>;
+    const domainPage = <?= json_encode($GLOBALS['domainPage']) ?>;
 
 
-courseFe.forEach(element => {
-    for (let i in element) {
-        if (!isNaN(Number(i))) {
-            delete element[i];
+    courseFe.forEach(element => {
+        for (let i in element) {
+            if (!isNaN(Number(i))) {
+                delete element[i];
+            }
         }
-    }
-});
-courseBe.forEach(element => {
-    for (let i in element) {
-        if (!isNaN(Number(i))) {
-            delete element[i];
+    });
+    courseBe.forEach(element => {
+        for (let i in element) {
+            if (!isNaN(Number(i))) {
+                delete element[i];
+            }
         }
-    }
-});
+    });
 
-chapters.forEach(element => {
-    for (let i in element) {
-        if (!isNaN(Number(i))) {
-            delete element[i];
+    chapters.forEach(element => {
+        for (let i in element) {
+            if (!isNaN(Number(i))) {
+                delete element[i];
+            }
         }
-    }
-});
+    });
 
-console.log(chapters)
 
-detail_courses.forEach(element => {
-    for (let i in element) {
-        if (!isNaN(Number(i))) {
-            delete element[i];
+    detail_courses.forEach(element => {
+        for (let i in element) {
+            if (!isNaN(Number(i))) {
+                delete element[i];
+            }
         }
-    }
-});
+    });
 
 
 
 
-// handle quantity btn pagination fe courses
-let numberData = 3
+    // handle quantity btn pagination fe courses
+    let numberData = 3
 
 
-const paginationFe = document.querySelector('.paginationFe')
-const paginationBe = document.querySelector('.paginationBe')
+    const paginationFe = document.querySelector('.paginationFe')
+    const paginationBe = document.querySelector('.paginationBe')
 
-for (let i = 0; i < Math.ceil(courseFe.length / numberData); i++) {
-    paginationFe.innerHTML += `
+    for (let i = 0; i < Math.ceil(courseFe.length / numberData); i++) {
+        paginationFe.innerHTML += `
         <button class="paginationFe-btn">${i + 1}</button>
     `
-}
+    }
 
-for (let i = 0; i < Math.ceil(courseBe.length / numberData); i++) {
-    paginationBe.innerHTML += `
+    for (let i = 0; i < Math.ceil(courseBe.length / numberData); i++) {
+        paginationBe.innerHTML += `
         <button class="paginationBe-btn">${i + 1}</button>
     `
-}
+    }
 
-// feat: pagination
+    // feat: pagination
 
-let temp = 0
+    let temp = 0
 
-const renderFe = (temp) => {
-    let target = temp > 0 ? temp * numberData : numberData
+    const renderFe = (temp, arr) => {
+        let target = temp > 0 ? temp * numberData : numberData
 
-    const newData = courseFe.slice(target - numberData, target)
+        const newData = arr.slice(target - numberData, target)
 
-    document.querySelector('.bodyFe').innerHTML = newData.map((course, index) => `
+        document.querySelector('.bodyFe').innerHTML = newData.map((course, index) => `
     <tr>
                                         <td>
                                         ${index + 1}
@@ -213,14 +209,14 @@ const renderFe = (temp) => {
                                         </td>
                                     </tr>
     `).join('')
-}
+    }
 
-const renderBe = (temp) => {
-    let target = temp > 0 ? temp * numberData : numberData
+    const renderBe = (temp, arr) => {
+        let target = temp > 0 ? temp * numberData : numberData
 
-    const newData = courseBe.slice(target - numberData, target)
+        const newData = arr.slice(target - numberData, target)
 
-    document.querySelector('.bodyBe').innerHTML = newData.map((course, index) => `
+        document.querySelector('.bodyBe').innerHTML = newData.map((course, index) => `
     <tr>
                                         <td>
                                         ${index + 1}
@@ -243,32 +239,32 @@ const renderBe = (temp) => {
                                         </td>
                                     </tr>
     `).join('')
-}
-
-renderFe(temp)
-renderBe(temp)
-
-// feat: click paginationFe-btn then pagination
-
-const btnsFe = document.querySelectorAll('.paginationFe-btn')
-btnsFe[0].classList.add("active")
-
-for (let i = 0; i < btnsFe.length; i++) {
-    btnsFe[i].onclick = () => {
-        document.querySelector(".paginationFe-btn.active").classList.remove("active")
-        btnsFe[i].classList.add("active")
-        renderFe(btnsFe[i].innerText)
     }
-}
 
-const btnsBe = document.querySelectorAll('.paginationBe-btn')
-btnsBe[0].classList.add("active")
-for (let i = 0; i < btnsBe.length; i++) {
-    btnsBe[i].onclick = () => {
-        document.querySelector(".paginationBe-btn.active").classList.remove("active")
-        btnsBe[i].classList.add("active")
-        renderBe(btnsBe[i].innerText)
+    renderFe(temp, courseFe)
+    renderBe(temp, courseBe)
+
+    // feat: click paginationFe-btn then pagination
+
+    const btnsFe = document.querySelectorAll('.paginationFe-btn')
+    btnsFe[0].classList.add("active")
+
+    for (let i = 0; i < btnsFe.length; i++) {
+        btnsFe[i].onclick = () => {
+            document.querySelector(".paginationFe-btn.active").classList.remove("active")
+            btnsFe[i].classList.add("active")
+            renderFe(btnsFe[i].innerText, courseFe)
+        }
     }
-}
+
+    const btnsBe = document.querySelectorAll('.paginationBe-btn')
+    btnsBe[0].classList.add("active")
+    for (let i = 0; i < btnsBe.length; i++) {
+        btnsBe[i].onclick = () => {
+            document.querySelector(".paginationBe-btn.active").classList.remove("active")
+            btnsBe[i].classList.add("active")
+            renderBe(btnsBe[i].innerText, courseBe)
+        }
+    }
 </script>
 <?php ipView("admin.component.footer") ?>
