954b49242a8c1fca2651b4282d17380303d49bac
TuanAnhNguyen <tuananh1210085213@gmail.com>
2023-04-09 21:58:07 +0700

    refactor: show-course

--- app/Views/client/pages/courses/index.php
+++ app/Views/client/pages/courses/index.php
@@ -29,34 +29,8 @@
 <div class="courses__wrapper">
     <div class="container">
         <h1 style="font-weight: 700; font-size: 28px; margin-bottom: 20px;">Khóa học Pro <span class="pro__label">Mới</span></h1>
-        <div class="row">
-            <?php foreach ($coursesPro as $key => $value) : ?>
+        <div data-course="3" class="row courseWrapper">
 
-
-                <div class="col-12 col-md-6 col-lg-3">
-                    <a href="<?= $GLOBALS["domainPage"] ?>/courses/detailCourse?courseId=<?= $value["id"] ?>">
-                        <div class="courses-newest_item">
-                            <div class="course__pro">
-                                <img src="<?= $GLOBALS['domainPage'] ?>/uploads/<?= $value["thumb"] ?>" alt="" />
-                                <span class="course__pro-icon">
-                                    <i class="fa-solid fa-crown"></i>
-                                </span>
-                            </div>
-                            <h4><?= $value["name"] ?></h4>
-                            <div class="courses-newest_info">
-                                <i class="fa-solid fa-users"></i>
-                                <span>123</span>
-                                <div class="price__wrapper">
-                                    <p class="old__price"><?= number_format($value['old_price'], 0, "", ".") ?>đđ</p>
-                                    <p><?= number_format($value['price'], 0, "", ".") ?>đ</p>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-
-                </div>
-
-            <?php endforeach ?>
         </div>
     </div>
     <div class="courses__category">
@@ -67,25 +41,9 @@
 <div class="courses__wrapper">
     <div class="container">
         <h1 style="font-weight: 700; font-size: 28px; margin-bottom: 20px;">Khóa học Front-End miễn phí</h1>
-        <div class="row" id="test">
-
-            <?php foreach ($coursesFe as $key => $value) : ?>
-                <div class="col-12 col-md-6 col-lg-3">
-                    <a href="<?= $GLOBALS["domainPage"] ?>/courses/detailCourse?courseId=<?= $value["id"] ?>">
-                        <div class="courses-newest_item">
-                            <img src="<?= $GLOBALS['domainPage'] ?>/uploads/<?= $value['thumb'] ?>" alt="" />
-                            <h4><?= $value['name'] ?></h4>
-                            <div class="courses-newest_info">
-                                <i class="fa-solid fa-users"></i>
-                                <span>123</span>
-                                <p>Miễn phí</p>
-                            </div>
-                        </div>
-                    </a>
-                </div>
+        <div data-course="1" class="row courseWrapper" id="test">
 
 
-            <?php endforeach ?>
         </div>
     </div>
 
@@ -94,24 +52,8 @@
 <div class="courses__wrapper">
     <div class="container">
         <h1 style="font-weight: 700; font-size: 28px; margin-bottom: 20px;">Khóa học Back-End miễn phí</h1>
-        <div class="row">
-            <?php foreach ($coursesBe as $key => $value) : ?>
-                <div class="col-12 col-md-6 col-lg-3">
-                    <a href="<?= $GLOBALS["domainPage"] ?>/courses/detailCourse?courseId=<?= $value["id"] ?>">
-                        <div class="courses-newest_item">
-                            <img src="<?= $GLOBALS['domainPage'] ?>/uploads/<?= $value['thumb'] ?>" alt="" />
-                            <h4><?= $value['name'] ?></h4>
-                            <div class="courses-newest_info">
-                                <i class="fa-solid fa-users"></i>
-                                <span>123</span>
-                                <p>Miễn phí</p>
-                            </div>
-                        </div>
-                    </a>
-                </div>
+        <div data-course="2" class="row courseWrapper">
 
-
-            <?php endforeach ?>
         </div>
     </div>
 
@@ -140,5 +82,60 @@
         </div>
     </div>
 </div>
+
+
+<script>
+    // handle get data from db and convert arr php to arr js
+    const courses = <?= json_encode($courses) ?>;
+
+    const detail_courses = <?= json_encode($detail_courses) ?>;
+
+
+
+    courses.forEach(element => {
+        for (let i in element) {
+            if (!isNaN(Number(i))) {
+                delete element[i];
+            }
+        }
+    });
+
+
+    detail_courses.forEach(element => {
+        for (let i in element) {
+            if (!isNaN(Number(i))) {
+                delete element[i];
+            }
+        }
+    });
+
+
+
+    const courseWrapper = document.querySelectorAll(".courseWrapper")
+
+    courseWrapper.forEach(item => {
+        const data = courses.filter(course => course.cate_id == item.dataset.course)
+        item.innerHTML = data.map(ele => `
+    <div class="col-12 col-md-6 col-lg-3">
+<a href="<?= $GLOBALS['domainPage'] ?>/courses/detailCourse?courseId=${ele.id}">
+                    <div class="courses-newest_item">
+                        <img src="<?= $GLOBALS['domainPage'] ?>/uploads/${ele.thumb}" alt="" />
+                        <h4>${ele.name}</h4>
+                        <div class="courses-newest_info">
+                            <i class="fa-solid fa-users"></i>
+                            <span>${detail_courses.filter(item => item.course_id == ele.id).length}</span>
+                            ${ele.price ?  `<div class="price__wrapper">
+                                <p class="old__price">${ele.old_price.toLocaleString()}đ</p>
+                                <p>${ele.price.toLocaleString()}đ</p>
+                            </div>` : `<p>Miễn phí</p>`}
+                            
+                           
+                        </div>
+                    </div>
+                </a>
+                </div>
+   `).join("")
+    })
+</script>
 
 <?php ipView('client.component.footer') ?>
